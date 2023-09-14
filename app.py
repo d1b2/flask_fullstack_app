d95f0f47dfd50d3a9ced55d1d0c27cd7acad0242
Dhruv Bakshi <dhruvbakshi20@gmail.com>
2023-09-14 11:55:14 +0530

    dynamic page title added

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template
-
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 #app routes
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',title='Home')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
 
 if __name__=='__main__':
     app.run(debug = True)
